8a4f193b4ef778ea4418cd601f413d72f8d3589f
Leonie <leonie.s.gasser@gmail.com>
2025-11-04 16:56:34 +0100

    logging and some error detection works:)

--- testcase.py
+++ testcase.py
@@ -1,11 +1,13 @@
-import threading
 import serial
 import csv
 import time
+import threading
 import os
+import io
+from contextlib import redirect_stdout
 from datetime import datetime
 import pandas as pd
-from ur_controller import UR
+from armcontrol.ur_controller import UR
 import numpy as np
 from FCT_Arduino_interface.plotting import plot_hist
 
@@ -13,10 +15,10 @@ from FCT_Arduino_interface.plotting import plot_hist
 SERIAL_PORT = 'COM7'
 BAUD_RATE = 500000
 SAVE_PATH = 'logs'
-LOG_TIME = 10
+LOG_TIME = 5
 PLOT = True
 UR_IP = '169.254.217.10'
-
+MOVE = [0.02, 0, 0, 0, 0, 0]
 
 # --- Data logging thread ---
 def log_data(stop_event, filename, timing, timing_lock):
@@ -42,14 +44,14 @@ def log_data(stop_event, filename, timing, timing_lock):
             finally:
                 ser.close()
                 print("Logging stopped")
-    except:
-        print("log failure: early disconnect")
+    except Exception as e:
+        print("log failure:", e)
 
 # --- Robot motion thread ---
 def move_robot(ur, timing, timing_lock):
     try:
         current_pose = ur.recv.getActualTCPPose()
-        new_pos = np.array(current_pose) + np.array([0.02, 0, 0, 0, 0, 0])
+        new_pos = np.array(current_pose) + np.array(MOVE)
         with timing_lock:
             timing['start_motion1'] = datetime.now()
         ur.move_pose_absolute(new_pos.tolist())
@@ -62,24 +64,37 @@ def move_robot(ur, timing, timing_lock):
         with timing_lock:
             timing['end_motion2'] = datetime.now()
         print("Robot motion done")
-    except:
-        print("movement failure")
+    except Exception as e:
+        print("movement failure:",e)
 
 # 1 for the forwards motion, -1 for the backwards motion, zero for no motion
 def motion_timing(t, timing):
-    if timing['start_motion1'] <= t <= timing['end_motion1']:
-        return 1
-    elif timing['start_motion2'] <= t <= timing['end_motion2']:
-        return -1
+    if ('start_motion1'in timing) and ('end_motion2' in timing):
+        if timing['start_motion1'] <= t <= timing['end_motion1']:
+            return 1
+        elif timing['start_motion2'] <= t <= timing['end_motion2']:
+            return -1
+        else:
+            return 0
     else:
         return 0
     
+    
 def main():
     # --- Init ---
+    thread_robo = True
     os.makedirs(SAVE_PATH, exist_ok=True)
     filename = os.path.join(SAVE_PATH, f"sensor_log_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.csv")
     ur = UR("UR5e", UR_IP)
-    ur.connect()
+
+    f = io.StringIO()
+    with redirect_stdout(f):
+        ur.connect()
+
+    output = f.getvalue()
+    if "cannot be connected" in output:
+        thread_robo = False
+
 
     # --- Thread stuff ---
     timing = {}
@@ -88,24 +103,29 @@ def main():
     log_thread = threading.Thread(target=log_data, args=(stop_event, filename, timing, timing_lock))
     log_thread.start()
 
-    motion_thread = threading.Thread(target=move_robot, args=(ur, timing, timing_lock))
-    motion_thread.start()
+    if thread_robo:
+        print('go_robo_thread')
+        motion_thread = threading.Thread(target=move_robot, args=(ur, timing, timing_lock))
+        motion_thread.start()
 
-    motion_thread.join()
-    stop_event.set()
+        motion_thread.join()
+        stop_event.set()
     log_thread.join()
 
     # --- Process data ---
-    df = pd.read_csv(filename)
-    df = df.fillna(0)
-    start_arduino = pd.to_numeric(df['Time'].iloc[0])
-    df["Time"] = datetime.fromtimestamp(timing['start_time']) + pd.to_timedelta(df["Time"] - start_arduino, unit="us")
-    df["Motion"] = df['Time'].apply(lambda t: motion_timing(t, timing))
-    df.to_csv(filename, index=False)
-    print(f"Finished. File: {filename}")
+    if ('start_time'in timing):
+        df = pd.read_csv(filename)
+        df = df.fillna(0)
+        start_arduino = pd.to_numeric(df['Time'].iloc[0])
+        df["Time"] = datetime.fromtimestamp(timing['start_time']) + pd.to_timedelta(df["Time"] - start_arduino, unit="us")
+        df["Motion"] = df['Time'].apply(lambda t: motion_timing(t, timing))
+        df.to_csv(filename, index=False)
+        print(f"Finished. File: {filename}")
 
-    if PLOT:
-        plot_hist(filename)
+        if PLOT:
+            plot_hist(filename)
+    else:
+        print('no logging happened')
 
 
 if __name__ == '__main__':
